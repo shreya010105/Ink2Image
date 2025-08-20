a5590498de1752a0b7fa3e3e8a5b618807a7202c
shreya <shreya.r2022cse@sece.ac.in>
2025-08-20 16:49:08 +0530

    Added Vercel frontend URL to CORS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ink-2-image.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 ConnectDB()
 
 app.use('/api/auth',userRoute)
